11d54380541ec539c3b41b5b2dc23aca29e6e2dd
LungoweA <lungowe3a@gmail.com>
2026-04-24 15:38:40 +0200

    more CRUD

--- server/controllers/dailyLogController.js
+++ server/controllers/dailyLogController.js
@@ -19,3 +19,49 @@ export const getLogs = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+export const getLogById = async (req, res) => {
+  try {
+    const log = await DailyLog.findById(req.params.id);
+
+    if (!log) {
+      return res.status(404).json({ message: "Log not found" });
+    }
+
+    res.json(log);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const updateLog = async (req, res) => {
+  try {
+    const log = await DailyLog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!log) {
+      return res.status(404).json({ message: "Log not found" });
+    }
+
+    res.json(log);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export const deleteLog = async (req, res) => {
+  try {
+    const log = await DailyLog.findByIdAndDelete(req.params.id);
+
+    if (!log) {
+      return res.status(404).json({ message: "Log not found" });
+    }
+
+    res.json({ message: "Log deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- server/routes/dailyLogRoutes.js
+++ server/routes/dailyLogRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createLog, getLogs } from "../controllers/dailyLogController.js";
+import {
+  createLog,
+  getLogs,
+  getLogById,
+  updateLog,
+  deleteLog
+} from "../controllers/dailyLogController.js";
 
 const router = express.Router();
 
 router.post("/", createLog);
 router.get("/", getLogs);
+router.get("/:id", getLogById);
+router.put("/:id", updateLog);
+router.delete("/:id", deleteLog);
 
 export default router;
